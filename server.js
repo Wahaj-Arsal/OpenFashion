9006a298be8d652bf8fe6dbd3a2ac5994a5a86f5
Wahaj-Arsal <83618649+Wahaj-Arsal@users.noreply.github.com>
2022-12-29 03:03:56 +0000

    Additional changes to Stripe API

--- server.js
+++ server.js
@@ -31,14 +31,19 @@ const CLIENT_PHONE_NUMBER = process.env.CLIENT_PHONE_NUMBER;
 app.listen(PORT, () => {
   console.log("Server Is Running " + `${PORT}`);
 });
+app.use(cors());
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", `${CLIENT_TOKEN}`);
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    `${process.env.REACT_APP_CLIENT_KEY}`
+  );
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
   next();
 });
+
 app.use(express.json());
 app.use(express.static(__dirname + "public"));
 
